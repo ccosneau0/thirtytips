ac6a63e44d370bf2bf88c0c44083617bec9ac48f
agordwin <agordwin@icloud.com>
2020-06-29 12:49:59 +0200

    router

---
+++ src/Routing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+//import {Router, Switch, Route} from "react-router-dom";
+import { Switch, BrowserRouter, Route, browserHistory} from 'react-router-dom';
+
+import App from "./App";
+import Registration from "./Pages/Registration/Registration";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+
+class Routing extends Component {
+render() {
+    return (
+        <BrowserRouter>
+
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route exact path='/registration' component={Registration} />
+                <Route exact path='/dashboard'  component={Dashboard} />
+            </Switch>
+
+        </BrowserRouter>
+
+    )
+}
+}
+
+
+export default Routing 
