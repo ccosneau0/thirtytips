cad48f918221ebbfee7ce39a35844967def7a80a
ccosneau0 <charles.cosneau@hetic.net>
2020-07-01 13:20:32 +0200

    switch dashboard to dashboard.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import './App.css';
-import UserInfo from './component/dashboard/UserInfo';
-import Tips from './component/dashboard/Tips';
-import DashArticle from './component/dashboard/DashArticle';
-import ToKnow from './component/dashboard/ToKnow';
-import Progress from './component/dashboard/Progress';
-
 
 function App() {
   return (
     <div className="App">
-      <section>
-        <UserInfo />
-        <ToKnow />
-        <Tips />
-        <DashArticle />
-        <Progress />
-      </section>
+
     </div>
   );
 }
