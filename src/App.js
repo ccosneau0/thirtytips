360dfc4624dba746ea1a3b1730982f8731b2bdba
agordwin <agordwin@icloud.com>
2020-07-07 16:04:16 +0200

    fix : app => landing  new design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import './App.scss';
 import './style/bigStyle.scss';
-import Logo from './images/LOGO.svg'
-import landimg from './images/landing.svg'
+import Logo from './images/LOGO.svg';
+import landimg from './images/landing.svg';
+import  fb from './images/socialMedia/fb.svg';
+import twitter  from './images/socialMedia/Twitter.svg';
+import linkedin from './images/socialMedia/Linkedin.svg';
+import insta from './images/socialMedia/insta.svg';
+import smallLogo from './images/socialMedia/small.svg';
 
 const App = () => {
   return (
     ////LANDING////
     <div className="App">
-      <div className="first-block">
+         
+      <div className="landing-cta-container">
+          <img className="logo" src={Logo} alt="okok"></img>
+         
+          <div className="text-wrapper">
+            <p>  
+              Adoptez facilement des gestes du quotidien  plus responsable pour limiter votre empreinte écologique. <br/>
+              <br/>Commencez dès maintenant et faites un pas de plus pour <br/>notre planète.
+            </p>
+          </div>
+          <button className="btn-log"> <i className="icon-avatared"></i> <a href={"/registration"} >crée ton compte</a></button>
+        </div>
+       
+        <div className="gris"> </div>
+        <footer className="footer"> 
+          <img className="logo" src={smallLogo} alt="okok"></img>
+          <p>Contact   -   Confidenialité   -   Conditions </p>
+          <div className="socialMedia">
+            <img  src={fb} alt="okok"></img>
+            <img src={insta} alt="okok"></img>
+            <img  src={linkedin} alt="okok"></img>
+            <img  src={twitter} alt="okok"></img>
+          </div>
+         </footer>
+         {/* NE PAS TOUCHER EN DESSOUS */}
+        <div className="landing-illustration">
+         <img src={landimg} alt="okok"></img> 
+      </div>
+    </div>
+  );
+};
+export default App;
+
+{/* <div className="first-block">
         <div className="landing-cta-container">
           <img className="logo" src={Logo} alt="okok"></img>
-          {/* <h1> 30 Steps to change</h1> */}
+         
           <div className="text-wrapper">
             <p>  
               Adoptez facilement des gestes du quotidien  plus responsable pour limiter votre empreinte  écologique. <br/>
@@ -23,10 +61,6 @@ const App = () => {
 
         <div className="landing-image-container">
           <img src={landimg} alt="okok"></img>
-        </div>
-      </div>
+        </div> 
+      </div> */}
           
-    </div>
-  );
-};
-export default App;
--- src/Component/Registration/Signup/Signup.js
+++ src/Component/Registration/Signup/Signup.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './Signup.scss';
 import Logo from '../../../images/LOGO.svg'
  
+
+ //@TODO : button disable => if input empty 
 function Signup() {
   return (
     <div className="Signup">
 
      {/*  ne pas prendre en compte button google => impélmentation a venir   */}
-      <img className="signup-logo"src={Logo}></img>
+     <img className="signup-logo"src={Logo}></img> 
       <h3> Sign up </h3>
-      <button className="btn-log"> GOOGLE not design </button>
-      <p>Or sign up with email</p>
-      <input type="email" value="entrez votre prénom/pseudo"></input>
-      <input type="email"></input>
-      <input type="password" ></input>
+     {/*  <button className="btn-log"> GOOGLE not design </button>
+      <p>Or sign up with email</p> */}
+      <label for="pseudo"></label>
+      <input type="text" id="pseudo" placeholder="entrez votre prénom/pseudo"></input>
+      <label for="email"></label>
+      <input type="email"  id="email" placeholder="entrez votre adresse e-mail"></input>
+      <label for="pwd"></label>
+      <input type="password" id="pwd" placeholder="entrez votre mot de passe" ></input>
       <button className="btn-log disabled"> <i className="icon-avatared"></i> <a href={"/dashboard"} >crée ton compte   </a></button>
       
       <p className="signup-policy">
