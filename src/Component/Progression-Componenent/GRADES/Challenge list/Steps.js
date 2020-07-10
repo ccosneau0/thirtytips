c2d50c642439b17370eecd591b8eda29930415dd
agordwin <agordwin@icloud.com>
2020-07-10 13:13:29 +0200

    fix progression

--- src/Component/Progression-Componenent/GRADES/Challenge list/Steps.js
+++ src/Component/Progression-Componenent/GRADES/Challenge list/Steps.js
@@ -4,8 +4,8 @@ import './Steps.scss';
 function Steps (){
   return(
     <div className="Steps">
-      <ul> Thème Name
-        <li> Step 1-  Se déplacer  </li>
+      <ul> <p>Énergie </p>
+        <li > Step 1-  Se déplacer  </li>
         <li> Step 2-  S'habiller</li>
         <li> Step 3- La créativité </li>
         <li> Step 4- la soif </li>
